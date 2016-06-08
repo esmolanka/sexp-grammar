3d64150e07f88a5f0a65bd31ab6221f04d81a433
Eugene Smolanka <esmolanka@gmail.com>
2016-06-08 22:36:22 +0300

    Add import of Typeable (needed for ghc-7.8)

--- bench/Main.hs
+++ bench/Main.hs
@@ -10,7 +10,7 @@ import Prelude hiding ((.), id)
 import Control.Arrow
 import Control.Category
 import qualified Data.ByteString.Lazy.Char8 as B8
-import Data.Data (Data)
+import Data.Data (Data, Typeable)
 import qualified Language.Sexp as Sexp
 import Language.SexpGrammar
 import Language.SexpGrammar.TH
