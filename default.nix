284ba2a41b752d2f7803067d302b8c28cfbd200f
Eugene Smolanka <esmolanka@gmail.com>
2021-10-10 00:58:10 +0100

    Remove commented out garbage from Nix definition

--- default.nix
+++ default.nix
@@ -1,10 +1,6 @@
+
 { compiler ? "ghc901" }:
 let
-  # nixpkgs = import (builtins.fetchTarball {
-  #   url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.05.tar.gz";
-  #   sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
-  # }) { inherit config; };
-
   nixpkgs = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/d432fcbf37034ba102197d6e0756e972600bdbe2.tar.gz";
     sha256 = "0lj809bc9y5xqc52kcrbcm6dc6nfa9icmw20r1d4hk51jp6550lf";
